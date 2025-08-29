ce9cb05d6b525949fc5d557da83ac17f42bf37b3
Idhant Jena <ijena@uci.edu>
2025-08-29 15:27:07 -0700

    Filterd out all the unwanted data from depth charts

--- src/nfl_verse_data.py
+++ src/nfl_verse_data.py
@@ -1,5 +1,5 @@
 import nfl_data_py
-
+import pandas as pd
 
 def load_depth_chart_data(years):
 
@@ -8,8 +8,20 @@ def load_depth_chart_data(years):
 
     #filter for fantasy relevant positions
     fantasy_positions = ["QB", "RB", "TE", "WR"]
-    return depth_charts[depth_charts["position"].isin(fantasy_positions)].copy()
+    
+    depth_charts = depth_charts.drop(columns = ["jersey_number"])
+    depth_charts = depth_charts[depth_charts["position"].isin(fantasy_positions)]
+    #filter for regular season games only
+    depth_charts = depth_charts[depth_charts["game_type"]=="REG"]
+    #filter for offense
+    depth_charts = depth_charts[depth_charts["formation"]=="Offense"]
 
+    depth_charts.to_csv(r"C:\Users\idhan\Downloads\Nerds with Numbers\fantasy-football-analysis-and-predictor\data\nflverse_data\depth_chart.csv")   
+
+def load_seasonal_data(years):
+    season_stats = nfl_data_py.import_seasonal_data(years)
+    season_stats = pd.DataFrame(season_stats)
+    season_stats.to_csv(r"C:\Users\idhan\Downloads\Nerds with Numbers\fantasy-football-analysis-and-predictor\data\nflverse_data\season_stats.csv")   
 years = range(2014,2025)
 depth_chart = load_depth_chart_data(years)
-
+load_seasonal_data(years)
