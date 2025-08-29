6625dc3f2e1e3a1afc04569fa84a17536e50b67b
Idhant Jena <ijena@uci.edu>
2025-08-29 00:37:42 -0700

    Converted code for depth chart data into a function

--- src/nfl_verse_data.py
+++ src/nfl_verse_data.py
@@ -1,13 +1,15 @@
 import nfl_data_py
 
+
+def load_depth_chart_data(years):
+
+
+    depth_charts = nfl_data_py.import_depth_charts(years)
+
+    #filter for fantasy relevant positions
+    fantasy_positions = ["QB", "RB", "TE", "WR"]
+    return depth_charts[depth_charts["position"].isin(fantasy_positions)].copy()
+
 years = range(2014,2025)
+depth_chart = load_depth_chart_data(years)
 
-depth_charts = nfl_data_py.import_depth_charts(years)
-print(sorted(depth_charts["position"].dropna().unique()))
-
-#filter for fantasy relevant positions
-fantasy_positions = ["QB", "RB", "TE", "WR"]
-depth_charts = depth_charts[depth_charts["position"].isin(fantasy_positions)].copy()
-
-
-print(sorted(depth_charts["position"].dropna().unique()))
