8f05dfa597714fb1a892526a11768f18dce8e44f
Idhant Jena <ijena@uci.edu>
2025-08-28 21:36:18 -0700

    Filtered depth charts for the fantasy relevant positions

--- src/nfl_verse_data.py
+++ src/nfl_verse_data.py
@@ -3,6 +3,11 @@ import nfl_data_py
 years = range(2014,2025)
 
 depth_charts = nfl_data_py.import_depth_charts(years)
+print(sorted(depth_charts["position"].dropna().unique()))
 
-print(depth_charts.columns)
-print(depth_charts.head())
+#filter for fantasy relevant positions
+fantasy_positions = ["QB", "RB", "TE", "WR"]
+depth_charts = depth_charts[depth_charts["position"].isin(fantasy_positions)].copy()
+
+
+print(sorted(depth_charts["position"].dropna().unique()))
