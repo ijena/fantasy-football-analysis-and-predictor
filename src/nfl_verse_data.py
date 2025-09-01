68e06ad73d7c607b113e582592b01801be218148
Idhant Jena <ijena@uci.edu>
2025-08-31 19:53:07 -0700

    Loaded player data

--- src/nfl_verse_data.py
+++ src/nfl_verse_data.py
@@ -52,6 +52,11 @@ def load_ngs_data (stat_type, years):
     ngs_data.to_csv(fr"C:\Users\idhan\Downloads\Nerds with Numbers\fantasy-football-analysis-and-predictor\data\nflverse_data\ngs_{stat_type}_data.csv")
     return ngs_data
 
+def load_player_data(fantasy_positions):
+    player_data = nfl_data_py.import_players()
+    player_data = player_data[player_data["position_group"].isin(fantasy_positions)]
+    player_data.to_csv(fr"C:\Users\idhan\Downloads\Nerds with Numbers\fantasy-football-analysis-and-predictor\data\nflverse_data\player_data.csv")
+    return player_data
 years = range(2014,2025)
 fantasy_positions = ["QB", "RB", "TE", "WR"]
 
@@ -62,5 +67,6 @@ draft_pick_data = load_draft_picks(years, fantasy_positions)
 ngs_data_passing = load_ngs_data("passing",years)
 ngs_data_rushing = load_ngs_data("rushing",years)
 ngs_data_receiving = load_ngs_data("receiving",years)
+player_data = load_player_data(fantasy_positions)
 
 
