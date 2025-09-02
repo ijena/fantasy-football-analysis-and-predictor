cd1245c4b0cd4dc2d003b0965afb55b7202bad2a
Idhant Jena <ijena@uci.edu>
2025-09-01 23:49:29 -0700

    Removed unnecessary play by play data

--- src/nfl_verse_data.py
+++ src/nfl_verse_data.py
@@ -87,15 +87,77 @@ def load_snap_counts(years,fantasy_positions):
 
 def load_play_by_play_data(years):
     play_by_play_data = nfl_data_py.import_pbp_data(years)
+    #drop irrelevant columns
+    play_by_play_data = play_by_play_data.drop(columns=["qb_kneel","qb_spike",
+    "pass_length","pass_location","field_goal_result","kick_distance",
+    "extra_point_result","two_point_conv_result","home_timeouts_remaining",
+    "away_timeouts_remaining","timeout","timeout_team","run_location","run_gap",
+    "posteam_timeouts_remaining","defteam_timeouts_remaining","total_home_score",
+    "total_away_score",	"posteam_score","defteam_score","score_differential",
+    "posteam_score_post","defteam_score_post","score_differential_post","no_score_prob",
+    "opp_fg_prob","opp_safety_prob","opp_td_prob","fg_prob","safety_prob","td_prob",
+    "extra_point_prob","two_point_conversion_prob","total_home_epa","total_away_epa",
+    "total_home_rush_epa","total_away_rush_epa","total_home_pass_epa","total_away_pass_epa",
+    "wp","def_wp","home_wp","away_wp","wpa","vegas_wpa","vegas_home_wpa","home_wp_post",
+    "away_wp_post","vegas_wp","vegas_home_wp","total_home_rush_wpa","total_away_rush_wpa",
+    "total_home_pass_wpa","total_away_pass_wpa","air_wpa","yac_wpa","comp_air_wpa",	
+    "comp_yac_wpa","total_home_comp_air_wpa","total_away_comp_air_wpa",
+    "total_home_comp_yac_wpa","total_away_comp_yac_wpa","total_home_raw_air_wpa",	
+    "total_away_raw_air_wpa","total_home_raw_yac_wpa","total_away_raw_yac_wpa",
+    "first_down_penalty","incomplete_pass",	"touchback","interception",
+    "punt_inside_twenty","punt_in_endzone","punt_out_of_bounds","punt_downed",	
+    "punt_fair_catch","kickoff_inside_twenty","kickoff_in_endzone",
+    "kickoff_out_of_bounds","kickoff_downed","kickoff_fair_catch", "fumble_forced",
+    "fumble_not_forced","fumble_out_of_bounds",	"solo_tackle","safety",	"penalty",
+    "tackled_for_loss","own_kickoff_recovery","own_kickoff_recovery_td","qb_hit",
+    "fumble_lost","return_touchdown","extra_point_attempt","two_point_attempt",
+    "field_goal_attempt","kickoff_attempt",	"punt_attempt","assist_tackle",
+    "lateral_reception","lateral_rush",	"lateral_return","lateral_recovery",
+    "lateral_receiver_player_id","lateral_receiver_player_name",
+    "lateral_receiving_yards","lateral_rusher_player_id","lateral_rusher_player_name",
+    "lateral_rushing_yards","lateral_sack_player_id","lateral_sack_player_name",
+    "interception_player_id","interception_player_name","lateral_interception_player_id",
+    "lateral_interception_player_name","punt_returner_player_id","punt_returner_player_name",
+    "lateral_punt_returner_player_id","lateral_punt_returner_player_name",
+    "kickoff_returner_player_name","kickoff_returner_player_id","lateral_kickoff_returner_player_id",
+    "lateral_kickoff_returner_player_name","punter_player_id","punter_player_name",	
+    "kicker_player_name","kicker_player_id","own_kickoff_recovery_player_id",
+    "own_kickoff_recovery_player_name",	"blocked_player_id","blocked_player_name",
+    "tackle_for_loss_1_player_id","tackle_for_loss_1_player_name","tackle_for_loss_2_player_id",	
+    "tackle_for_loss_2_player_name","qb_hit_1_player_id","qb_hit_1_player_name","qb_hit_2_player_id",
+    "qb_hit_2_player_name","forced_fumble_player_1_team","forced_fumble_player_1_player_id",
+    "forced_fumble_player_1_player_name","forced_fumble_player_2_team",	
+    "forced_fumble_player_2_player_id","forced_fumble_player_2_player_name","solo_tackle_1_team",
+    "solo_tackle_2_team","solo_tackle_1_player_id",	"solo_tackle_2_player_id","solo_tackle_1_player_name",
+    "solo_tackle_2_player_name","assist_tackle_1_player_id","assist_tackle_1_player_name","assist_tackle_1_team",
+    "assist_tackle_2_player_id","assist_tackle_2_player_name","assist_tackle_2_team","assist_tackle_3_player_id",
+    "assist_tackle_3_player_name","assist_tackle_3_team","assist_tackle_4_player_id","assist_tackle_4_player_name",
+    "assist_tackle_4_team","tackle_with_assist","tackle_with_assist_1_player_id","tackle_with_assist_1_player_name",
+    "tackle_with_assist_1_team","tackle_with_assist_2_player_id","tackle_with_assist_2_player_name",
+    "tackle_with_assist_2_team","pass_defense_1_player_id",	"pass_defense_1_player_name","pass_defense_2_player_id",
+    "pass_defense_2_player_name","fumbled_1_team","fumbled_1_player_id","fumbled_1_player_name","fumbled_2_player_id",
+    "fumbled_2_player_name","fumbled_2_team","fumble_recovery_1_team","fumble_recovery_1_yards","fumble_recovery_1_player_id",
+    "fumble_recovery_1_player_name","fumble_recovery_2_team","fumble_recovery_2_yards",	"fumble_recovery_2_player_id",
+    "fumble_recovery_2_player_name","sack_player_id","sack_player_name","half_sack_1_player_id","half_sack_1_player_name",
+    "half_sack_2_player_id","half_sack_2_player_name","return_team","return_yards","penalty_team","penalty_player_id",
+    "penalty_player_name","penalty_yards","replay_or_challenge","replay_or_challenge_result	penalty_type",
+    "defensive_two_point_attempt","defensive_two_point_conv","defensive_extra_point_attempt","defensive_extra_point_conv",
+    "safety_player_name","safety_player_id","series","series_success","series_result","order_sequence",	"start_time",
+    "time_of_day","stadium","weather","play_clock","play_deleted","play_type_nfl","special_teams_play",	"st_play_type",
+    "end_clock_time","end_yard_line","fixed_drive",	"fixed_drive_result","drive_real_start_time", "drive_quarter_start",
+    "drive_quarter_end","drive_yards_penalized","drive_start_transition","drive_end_transition","drive_game_clock_start",
+    "drive_game_clock_end",	"drive_start_yard_line","drive_end_yard_line","drive_play_id_started","drive_play_id_ended",
+    "away_score","home_score","location","result","home_coach","away_coach","stadium_id","game_stadium","aborted_play",
+    "success","passer_jersey_number","rusher_jersey_number","receiver_jersey_number","special", "jersey_number",
+    "out_of_bounds","home_opening_kickoff",	"possession_team",	"offense_personnel","defense_personnel","number_of_pass_rushers",
+    "offense_names","defense_names","offense_positions","defense_positions","offense_numbers","defense_numbers"])
     #filter for regular season game
+
     play_by_play_data = play_by_play_data[play_by_play_data["season_type"]=="REG"]
     #filter for relevant plays
     play_by_play_data = play_by_play_data[play_by_play_data["play_type"].isin["run","pass"]]
-
-    for col in play_by_play_data.columns:
-        print(col)
         
-    # play_by_play_data.to_csv(r"C:\Users\idhan\Downloads\Nerds with Numbers\fantasy-football-analysis-and-predictor\data\nflverse_data\play_by_play_data.csv")
+    play_by_play_data.to_csv(r"C:\Users\idhan\Downloads\Nerds with Numbers\fantasy-football-analysis-and-predictor\data\nflverse_data\play_by_play_data.csv")
     return play_by_play_data
 
 years = range(2014,2025)
