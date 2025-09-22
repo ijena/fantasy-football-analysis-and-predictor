2ffdb3fa8a87165ad4428d777cd4bf43c4729b3b
Idhant Jena <ijena@uci.edu>
2025-09-21 23:06:08 -0700

    Converted the average probability for each classification into a percentage and rounded it to 2 decimal places

--- src/processing_predicted_data.py
+++ src/processing_predicted_data.py
@@ -20,3 +20,10 @@ predicted_df["average_probability_under"] = predicted_df[["xgb_classification_pr
 
 predicted_df["average_probability_neutral"] = predicted_df[["xgb_classification_prob_neutral","xgb_percentile_prob_neutral",
                                                          "rf_classification_prob_neutral","rf_percentile_prob_neutral"]].mean(axis=1)
+
+#convert all the averages to percentages and round it to 2 decimal places
+
+predicted_df["average_probability_over"] = (predicted_df["average_probability_over"] * 100).round(2)
+predicted_df["average_probability_under"] = (predicted_df["average_probability_under"] * 100).round(2)
+predicted_df["average_probability_neutral"] = (predicted_df["average_probability_neutral"] * 100).round(2)
+
