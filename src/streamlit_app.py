8e050cfda50815269bf07d1d4768a352db4a1557
Idhant Jena <ijena@uci.edu>
2025-09-26 15:59:21 -0700

    Modified examples

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -95,9 +95,9 @@ with colL:
 
 with colR:
     st.markdown("**Examples**")
-    st.code("top 10 predicted quarterback overperformers for 2025")
     st.code("worst 15 underperformers in 2019")
     st.code("Average Draft Position (ADP) of Joe Burrow from 2021 - 2025")
+    st.code("top 10 predicted underperforming wide receivers with an adp lower than 50 in 2025")
 
 run = st.button("Run")
 
